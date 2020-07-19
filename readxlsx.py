db7a413e88c76172d4d9afd25882618bbc9b84e0
anton <ant.monakhov@gmail.com>
2020-07-19 12:37:07 +0300

    alpha 0.1

--- readxlsx.py
+++ readxlsx.py
@@ -19,8 +19,9 @@ def mk_df(file):
         while type(wb[sheetname]['A' + str(offset)].value) not in dtypes:
             offset += 1
         for row in (wb[sheetname]['A1:Z' + str(offset-1)]):
-            for cell in row:
-                print(row)
+            for i in range(row):
+                cell = row[i]
+                print(cell)
                 if type(cell.value) is str:
                     for name in (snames + enames):
                         if name in cell.value.lower():
